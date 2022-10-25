2998f005e1b356c5924f26e36e3b8262c6e11d2a
sye344 <116538575+sye344@users.noreply.github.com>
2022-10-25 22:04:57 +0530

    Update sexyy.c

--- sexyy.c
+++ sexyy.c
@@ -1,116 +1,539 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-char current[256];
-void cd(char *path) {
-	int ret;
-	if (path[0] == '.') {
-		getcwd(current, sizeof(current));
-		strcat(current,"/");
-		strcat(current,path);
-		ret = chdir(current);	
-	}
-	else if (path[0] == '~') {
-		path++;
-		char *current = getenv("HOME");
-		strcat(current, path);
-		ret = chdir(current);
-	}
-	else {
-		ret = chdir(path);
-	}
-	if (ret == -1) {
-		printf("Invalid Path\n");
-	}
-	else {
-		char present[10001];
-		getcwd(present, sizeof(present));
-		printf("%s\n", present);
-	}
-}
-char **split_line(char *line)
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+char commandPath[100];
+char history_path[100] = {"/home/"};
+#define clear() printf("\033[H\033[J")
+pid_t pid;
+char int_command[5][200] = {"cd", "echo", "history", "pwd", "exit"};
+char ext_command[5][200] = {"ls", "cat", "date", "rm", "mkdir"};
+void performInternal(char *);
+void performExternal(char *);
+char *username;
+
+void printHistory(char flag[])
 {
-    int len = 0;
-    int cap = 16;
-    char **split_lines = malloc(cap * sizeof(char *));
-
-    char *del = " \t\r\n";
-    char *token = strtok(line, del);
-
-    while (token != NULL)
+    if (strcmp(flag, "-c") == 0 || strcmp(flag,"-w")==0) // to remove all history
     {
-        split_lines[len] = token;
-        len++;
+        char h_cmd[100]={"cd /home/"};
+        strcat(h_cmd,username);
+        strcat(h_cmd,"&& rm .bash_history");
+        // performInternal("cd /home/shubham-lohan/");
+        // performExternal("rm .bash_history");
+        system(h_cmd);
 
-        if (len >= cap)
+        return;
+    }
+    FILE *file;
+    char display[100];
+    file = fopen(history_path, "a+");
+    int counter = 1;
+    while (fgets(display, 100, file) != NULL)
+    {
+        printf("%d  %s\n", counter++, display);
+    }
+    fclose(file);
+}
+void runecho(char flag[], char *word)
+{
+    if (strcmp(flag, "-n") == 0)
+    {
+        while (word != NULL)
         {
-            cap = (int)(cap * 1.5);
-            split_lines = realloc(split_lines, cap * sizeof(char *));
+            printf("%s", word);
+            word = strtok(NULL, " ");
         }
+        return;
+    }
+    while (word != NULL)
+    {
+        // printf("outside: %s\n", word);
+        word = strtok(NULL, " ");
+    }
+}
+int checkcommand(char command[])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (strcmp(command, int_command[i]) == 0)
+        {
+            return 0;
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        if (strcmp(command, ext_command[i]) == 0)
+        {
+            return 1;
+        }
+    }
+    return -1;
+}
+void runDate(char *command)
+{
+    // char *const argv[]={"./date","-u",NULL};
+    int index = 0;
+    char *temp = command;
+    int flag = 0;
+    int flag_u = 0;
+    int flag_r = 0;
+    int counter = 0;
+    char path[100];
+    strcpy(path, commandPath);
+    strcat(path, "/date");
+    while (temp != NULL)
+    {
+        if (strcmp(temp, "-u") == 0)
+        {
+            flag_u = 1;
+        }
+        if (strcmp(temp, "-R") == 0)
+        {
+            flag_r = 1;
+        }
+        temp = strtok(NULL, " ");
+        counter++;
+    }
+    if (counter < 2)
+    {
+        flag = 1;
+    }
+    if (flag_u == 1 && flag_r == 1)
+    {
+        printf("Enter correct commmand\n");
+        return;
+    }
+    if (flag)
+    {
+        char *const argv[] = {path, NULL};
+        pid = fork();
+        if (pid == 0)
+        {
+            flag = 0;
+            // printf("date command is called\n")s;
+            execv(argv[0], argv);
+            // perror("Return from execv() not expected");
+            // printf("date command is finished\n");
+            return;
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+    if (flag_u)
+    {
+        char *const argv[] = {path, "-u", NULL};
+        pid = fork();
+        if (pid == 0)
+        {
+            flag_u = 0;
+            // printf("date command is called\n");
+            execv(argv[0], argv);
+            // perror("Return from execv() not expected");
+            // printf("date command is finished\n");
+            return;
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+    if (flag_r)
+    {
+        flag_r = 0;
+        char *const argv[] = {path, "-R", NULL};
+        pid = fork();
+        if (pid == 0)
+        {
+            // printf("date command is called\n");
+            execv(argv[0], argv);
+            // perror("Return from execv() not expected");
+            // printf("date command is finished\n");
+            return;
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+}
+void runLS(char *command)
+{
+    int index = 0;
+    int flag = 0;
+    int flag_a = 0;
+    int flag_m = 0;
+    int counter = 0;
+    char path[100];
+    strcpy(path, commandPath);
+    strcat(path, "/ls");
+    while (command != NULL)
+    {
+        if (strcmp(command, "-a") == 0)
+        {
+            flag_a = 1;
+        }
+        if (strcmp(command, "-m") == 0)
+        {
+            flag_m = 1;
+        }
+        command = strtok(NULL, " ");
+        counter++;
+    }
+    if (counter < 2)
+    {
+        flag = 1;
+    }
+    if (flag_a == 1 && flag_m == 1)
+    {
+        printf("Enter correct commmand");
+        return;
+    }
+    if (flag == 1)
+    {
 
-        token = strtok(NULL, del);
+        // printf("%s", path);
+        char *const argv[] = {path, NULL};
+        pid = fork();
+        if (pid == 0)
+        {
+            execv(argv[0], argv);
+        }
+        else
+        {
+            wait(NULL);
+            return;
+        }
+    }
+    if (flag_a)
+    {
+        //  printf("ls -a is called\n");
+
+        char *const argv[] = {path, "-a", NULL};
+        pid = fork();
+        if (pid == 0)
+        {
+            execv(argv[0], argv);
+        }
+        else
+        {
+            wait(NULL);
+            return;
+        }
+    }
+    if (flag_m)
+    {
+        //  printf("ls -m is called\n");
+        char *const argv[] = {path, "-m", NULL};
+        pid = fork();
+        if (pid == 0)
+        {
+            execv(argv[0], argv);
+        }
+        else
+        {
+            wait(NULL);
+            return;
+        }
+    }
+    printf("Enter right command\n");
+    return;
+}
+void runCat(char *command)
+{
+    char *argv[10];
+    int index = 0;
+    char path[100];
+    strcpy(path, commandPath);
+    strcat(path, "/cat");
+    for (int i = 0; i < 10; i++)
+        argv[i] = (char *)malloc(100 * sizeof(char));
+    strcpy(argv[index++], path);
+    while (command != NULL)
+    {
+        if (strcmp(command, "cat") == 0)
+        {
+            command = strtok(NULL, " ");
+            continue;
+        }
+        strcpy(argv[index++], command);
+        command = strtok(NULL, " ");
+    }
+    argv[index++] = NULL;
+    // for (int i=0; i<10; i++)
+    //     printf("%s ",argv[i]);
+
+    pid = fork();
+    if (pid == 0)
+    {
+        // printf("cat called\n");
+        execv(argv[0], argv);
+        // printf("cat finished\n");
+        return;
+    }
+    else
+    {
+        wait(NULL);
+        return;
+    }
+}
+void runRM(char *command)
+{
+    char *argv[10];
+    int index = 0;
+    char path[100];
+    strcpy(path, commandPath);
+    strcat(path, "/rm");
+    for (int i = 0; i < 10; i++)
+        argv[i] = (char *)malloc(100 * sizeof(char));
+    strcpy(argv[index++], path);
+    while (command != NULL)
+    {
+        if (strcmp(command, "rm") == 0)
+        {
+            command = strtok(NULL, " ");
+            continue;
+        }
+        strcpy(argv[index++], command);
+        command = strtok(NULL, " ");
+    }
+    argv[index++] = NULL;
+    // for (int i=0; i<10; i++)
+    //     printf("%s ",argv[i]);
+
+    pid = fork();
+    if (pid == 0)
+    {
+        // printf("cat called\n");
+        execv(argv[0], argv);
+        // printf("cat finished\n");
+        return;
+    }
+    else
+    {
+        wait(NULL);
+        return;
+    }
+}
+void runMkdir(char *command)
+{
+    char *argv[10];
+    int index = 0;
+    char path[100];
+    strcpy(path, commandPath);
+    strcat(path, "/mkdir");
+    for (int i = 0; i < 10; i++)
+        argv[i] = (char *)malloc(100 * sizeof(char));
+    strcpy(argv[index++], path);
+    while (command != NULL)
+    {
+        if (strcmp(command, "mkdir") == 0)
+        {
+            command = strtok(NULL, " ");
+            continue;
+        }
+        strcpy(argv[index++], command);
+        command = strtok(NULL, " ");
+    }
+    argv[index++] = NULL;
+    // for (int i=0; i<10; i++)
+    //     printf("%s ",argv[i]);
+
+    pid = fork();
+    if (pid == 0)
+    {
+        // printf("cat called\n");
+        execv(argv[0], argv);
+        // printf("cat finished\n");
+        return;
+    }
+    else
+    {
+        wait(NULL);
+        return;
+    }
+}
+void performExternal(char *command)
+{
+    if (strcmp(command, "date") == 0)
+    {
+        runDate(command);
+        return;
+    }
+    if (strcmp(command, "ls") == 0)
+    {
+        runLS(command);
+    }
+    if (strcmp(command, "cat") == 0)
+    {
+        runCat(command);
+    }
+    if (strcmp(command, "rm") == 0)
+    {
+        runRM(command);
     }
 
-    split_lines[len] = NULL;
-    return split_lines;
+    if (strcmp(command, "mkdir") == 0)
+    {
+        runMkdir(command);
+    }
 }
-int main()
+
+void performInternal(char *command)
 {
 
-    printf("This Shell is created by Mahir\n");
-    
-    while (true)
+    if (strcmp(command, "pwd") == 0)
     {
-        printf("$ ");
-        char *line = NULL;
-        size_t bufferlength = 0;
-        getline(&line, &bufferlength, stdin);
-        char **split_lines = split_line(line);
-        bool flag =false;
-        for(int i=0;i<strlen(line);i++){
-            if(line[i]=="&" && line[i+1]=="t"){
-                flag=true;
-            }
-        }
-        if(!flag){
-            if (split_lines[0] != NULL)
+        char cwd[100];
+        puts(getcwd(cwd, sizeof(cwd)));
+        return;
+    }
+    if (strcmp(command, "history") == 0)
+    {
+        while (command != NULL)
+        {
+            if (strcmp(command, "-c") == 0)
             {
-                if(strcmp(split_lines[0],"cd")==0){
-                    cd(split_lines[1]);
-                    continue;
-                }
-                pid_t child = fork();
-
-                if (child == 0)
-                {
-                    execvp(split_lines[0], split_lines);
-                    perror("Error: ");
-                    exit(1);
-                }
-                else if (child > 0)
-                {
-                    int status;
-                    do
-                    {
-                        waitpid(child, &status, WUNTRACED);
-                    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-                }
-                else
-                {
-                    perror("Error: ");
-                }
-                
+                printHistory("-c");
+                return;
             }
+            command = strtok(NULL, " ");
         }
-        else{
-            printf("hi");
+        printHistory(" ");
+        return;
+    }
+    if (strcmp(command, "echo") == 0)
+    {
+        char string[1000][1000];
+        int index = 0;
+        char flag[10];
+        while (command != NULL)
+        {
+            if (index == 1 && strcmp(command, "-n") == 0)
+            {
+                strcpy(flag, "-n");
+            }
+             if (index == 1 && strcmp(command, "--help") == 0)
+            {
+                strcpy(flag, "--help");
+            }
+            strcpy(string[index++], command);
+            command = strtok(NULL, " ");
+        }
+        if (strcmp(flag, "-n") == 0)
+        {
+            for (int i = 2; i < index; i++)
+            {
+                printf("%s ", string[i]);
+            }
+            return;
+        }
+         if (strcmp(flag, "--help") == 0)
+        {
+            printf("Echo the STRING(s) to standard output.\n-n do not output the trailing newline\n--help display this help and exit");
+            return;
         }
 
-        
-        free(split_lines);
-        free(line);
+
+        for (int i = 1; i < index; i++)
+        {
+            printf("%s ", string[i]);
+        }
+        printf("\n");
+        return;
+    }
+    if (strcmp(command, "cd") == 0)
+    {
+        char path[10][100];
+        int index = 0;
+        while (command != NULL)
+        {
+            if (strcmp(command, "-L") == 0)
+            {
+
+                command = strtok(NULL, " ");
+                continue;
+            }
+            strcpy(path[index++], command);
+            command = strtok(NULL, " ");
+        }
+        int c = chdir(path[1]);
+        if (c == -1)
+        {
+            printf(" No such file or directory\n");
+        }
     }
 }
+int main() // start of the shell command prompt
+{
+    clear();
+    int pid;
+    char userCmdInput[100];
+    getcwd(commandPath, sizeof(commandPath));
+    username = getenv("USER");
+    if (username == NULL)
+        return EXIT_FAILURE;
+    // printf("%s\n", username);
+    strcat(history_path,username);
+    strcat(history_path,"/.bash_history");
+
+    // strcat(history_path, system("echo $USER"));
+    // strcat(history_path,"/.bash_history");
+    // printf("path %s", history_path);
+    // printf("%s\n",username );
+
+    while (1) // This is a infinite loop which won't terminate till it encounters a exit code zero;
+    {
+        char cwd[100];
+        printf("%s@ubuntu:%s:~$ ",username,getcwd(cwd, sizeof(cwd))); //prompt for the command shell
+        // scanf("%s",&userCmdInput);
+
+        gets(userCmdInput);
+        FILE *file;
+
+        file = fopen(history_path, "a+");
+        fputs(userCmdInput, file);
+        fputs("\n", file);
+        fclose(file);
+
+        if (strlen(userCmdInput) == 0)
+            continue;
+        if (strcmp(userCmdInput, "cls") == 0)
+        {
+            clear();
+            continue;
+        }
+        char *split = strtok(userCmdInput, " ");
+        if (strcmp(userCmdInput, "exit") == 0)
+        {
+
+            return 0;
+        }
+        if (checkcommand(split) == -1)
+        {
+            puts("please enter right command\n");
+            continue;
+        }
+        if (checkcommand(split) == 0)
+        {
+            // puts("Internal Command\n");
+            performInternal(split);
+            continue;
+        }
+        if (checkcommand(split) == 1)
+        {
+            // puts("External Command\n");
+            performExternal(split);
+            continue;
+        } //gets the users input
+
+        // system(userCmdInput); // parses the user input to the system and creates a new child process that executes the command given by the user and returns the value to the parent process which is the main code of out program
+    }
+    return 0;
+}
+
